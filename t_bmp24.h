d4658b4b63cb661c48bd8ccfadb66c127433f2db
Imad-eddine-Mahraz <imad-eddine.mahraz@efrei.net>
2025-05-09 11:47:30 +0200

    fihicer bmp24

--- t_bmp24.h
+++ t_bmp24.h
@@ -73,5 +73,22 @@ void bmp24_outline(t_bmp24 *img);
 void bmp24_emboss(t_bmp24 *img);
 void bmp24_sharpen(t_bmp24 *img);
 
+
+// ============================================== Constante ==============================================================
+#define BITMAP_MAGIC 0x00 // offset 0
+#define BITMAP_SIZE 0x02 // offset 2
+#define BITMAP_OFFSET 0x0A // offset 10
+#define BITMAP_WIDTH 0x12 // offset 18
+#define BITMAP_HEIGHT 0x16 // offset 22
+#define BITMAP_DEPTH 0x1C // offset 28
+#define BITMAP_SIZE_RAW 0x22 // offset 34
+// Constante pour le type de fichier BMP
+#define BMP_TYPE 0x4D42 // 'BM' en hexadécimal
+// Constantes pour les tailles des champs de l'en-tête BMP
+#define HEADER_SIZE 0x0E // 14 octets
+#define INFO_SIZE 0x28 // 40 octets
+// Constantes pour les valeurs de profondeur de couleur
+#define DEFAULT_DEPTH 0x18 // 24
+
 #endif // BMP24_H
 
